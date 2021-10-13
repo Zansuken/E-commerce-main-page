8e1b975b0ea72d01801b733dfd9ab437a9624e6c
Amélys Krifa <doom.sebastien.pro@gmail.com>
2021-10-13 16:13:56 +0200

    open and close side panel script added

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Carousel
+// Side panel
+
+const hamburgerBtn = document.getElementById("hamburger_button");
+const crossBtn = document.getElementById("cross_button");
+let sidePanel = document.getElementById("side_panel");
+
+function openSidePanel() {
+    sidePanel.style.transform = "translateX(0%)";
+}
+
+function closeSidePanel() {
+    sidePanel.style.transform = "translateX(-100%)";
+}
+hamburgerBtn.onclick = openSidePanel;
+crossBtn.onclick = closeSidePanel;
